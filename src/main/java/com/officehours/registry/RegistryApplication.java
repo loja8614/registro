07cd3204b3eed0e41cf1306cc10e1a2b630725d0
jazmin <jazmin.lopez@nielsen.com>
2022-08-19 09:57:48 -0500

    org.springdoc

--- src/main/java/com/officehours/registry/RegistryApplication.java
+++ src/main/java/com/officehours/registry/RegistryApplication.java
@@ -3,10 +3,12 @@ package com.officehours.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
-public class RegistryApplication {
+
+public class RegistryApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryApplication.class, args);
 	}
